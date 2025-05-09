d555432ddd2ef09889eb243ba407cf623cfaf40c
ajay-deshmukh24 <ajayadeshmukh2003@gmail.com>
2025-05-09 04:57:35 +0000

    proxy server and client accept functionality done

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -63,7 +63,17 @@ int main(int argc,char* argv[]){
 
     int client_socketId; // stores socketId of client wants to connect
     int client_len; //store length of adress
+
     struct sockaddr_in server_addr,client_addr; //address of client and server to be assigned
+
+    /*
+        struct sockaddr_in {
+            sa_family_t    sin_family;   // Address family (AF_INET for IPv4)
+            in_port_t      sin_port;     // Port number (must be in network byte order)
+            struct in_addr sin_addr;     // IP address (in struct form)
+            char           sin_zero[8];  // Padding (not used, just to match struct size with sockaddr)
+        };
+    */
 
     sem_init(&semaphore,0,MAX_CLIENTS); //initialize semaphore
     // 0 --> minimum value
@@ -83,6 +93,9 @@ int main(int argc,char* argv[]){
 
     printf("Starting proxy server at port &d\n",port_number);
 
+    
+    // create the socket
+    
     // open listening socket on primary side of proxy server
     proxy_socketId = socket(AF_INET,SOCKET_STREAM,0);
     // creates TCP socket that uses IPv4
@@ -96,8 +109,78 @@ int main(int argc,char* argv[]){
     }
 
     int reuse = 1;
-    // check if we can reuse same port
+    // check if we can reuse same adress
     if (setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse)) < 0) 
     perror("setsockopt(SO_REUSEADDR) failed\n");
 
+    bzero((char*)&server_addr,sizeof(server_addr));
+    server_addr.sin_family = AF_INET;  // communication over an IPv4 network
+    server_addr.sin_port = htons(port_number); //assign port to proxy
+    server_addr.sin_addr.s_addr = INADDR_ANY; // any available address assigned
+
+
+    // Binding the socket
+
+    if(bind(proxy_socketId,(struct sockaddr*)&server_addr,sizeof(server_addr)) < 0){
+        perror("Port is not free\n");
+        exit(1);
+    }
+    printf("binding on port: %d\n",port_number);
+
+
+    // proxy socket listening to the requests
+
+    int listen_status = listen(proxy_socketId,MAX_CLIENTS);
+
+    if(listen_status < 0){
+        perror("Error while Listening !\n");
+        exit(1);
+    }
+
+
+    int i=0; //Iterator for thread_id(tid) and Accept client_socket for each thread
+    int Connected_socketId[MAX_CLIENTS]; //this array stores sockket descriptors of connected clients
+
+    // Infinite Loop for acceoting clients
+    while(1){
+        bzero((char*)&client_addr,sizeof(client_addr));
+
+        // Accepting the connections
+        client_socketId = accept(proxy_socketId,(struct socaddr*)&client_addr,(socklen_t*)&client_len);
+
+        // NOTE : accept() waits forever if there is no client to connect currently and do not give error
+
+        if(client_socketId < 0){
+            fprintf(stderr,"Error in Accepting connection !\n");
+            exit(1);
+        }
+        else{
+            Connected_socketId[i] = client_socketId; //store accepted client into array
+        }
+
+        //Getting Ip adress and port number of client
+        struct sockaddr_in* client_pt = (struct sockaddr_in*)& client_addr;
+
+        /*
+            struct in_addr {
+                unsigned long s_addr;    // IP address in binary (network byte order)
+            };
+        */
+        struct in_addr ip_addr = client_pt->sin_addr;
+        
+        // allocate character array with size sufficient to store any valid IPv4 address
+        // INET_ADDRSTRLEN: Default ip address size
+        char str[INET_ADDRSTRLEN];
+
+        // convert binary form of IPv4 address into string
+        inet_ntop(AF_INET,&ip_addr,str,INET_ADDRSTRLEN);
+        printf("Client is connected with port number: %d and ip adress: %s\n",ntohs(client_addr.sin_port),str);
+
+        // creating thread for each accepted client
+        pthread_create(&tid[i],NULL,thread_fn,(void*)&Connected_socketId[i]); 
+        i++;
+    }
+
+    close(proxy_socketId);
+    return 0;
 }
